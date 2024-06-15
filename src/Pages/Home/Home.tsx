6dbcf8638a143bb5cc123d93802b656f3340bb3b
Vhs4 <vhszera44444@gmail.com>
2024-06-15 20:51:28 -0300

    FEAT: Edição final

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -6,26 +6,24 @@ const Home: React.FC = () => {
   const [sequence, setSequence] = useState<string[]>([]);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [gameOver, setGameOver] = useState<boolean>(false);
-  const [timeLeft, setTimeLeft] = useState<number>(20); // Tempo inicial de 20 segundos
-  const [score, setScore] = useState<number>(0); // Estado para a pontuação
-  const [username, setUsername] = useState<string>(''); // Estado para o nome de usuário
-  const [password, setPassword] = useState<string>(''); // Estado para a senha
-  const [loggedIn, setLoggedIn] = useState<boolean>(false); // Estado para controlar o login
-  const [usersScores, setUsersScores] = useState<{ username: string; score: number }[]>([]); // Estado para armazenar pontuações de usuários
-  const [showModal, setShowModal] = useState<boolean>(false); // Estado para controlar a exibição do modal
+  const [timeLeft, setTimeLeft] = useState<number>(20);
+  const [score, setScore] = useState<number>(0);
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [usersScores, setUsersScores] = useState<{ username: string; score: number }[]>([]);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [showCreateModal, setShowCreateModal] = useState<boolean>(false);
 
   useEffect(() => {
-    // Carregar a pontuação salva no localStorage ao iniciar o componente
     const savedScore = localStorage.getItem(`${username}-score`);
     if (savedScore) {
       setScore(parseInt(savedScore, 10));
     }
 
-    // Carregar todas as pontuações salvas no localStorage
     const scores = getAllUsersScores();
     setUsersScores(scores);
 
-    // Verificar se já há um usuário logado
     const savedUsername = localStorage.getItem('username');
     if (savedUsername) {
       setUsername(savedUsername);
@@ -49,37 +47,38 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
-      if (event.key === 'Escape') {
-        if (loggedIn) {
-          if (window.confirm('Deseja sair do usuário?')) {
-            handleLogout();
-          }
+      if (loggedIn && !gameOver && event.key === 'Escape') {
+        if (window.confirm('Deseja sair do usuário?')) {
+          handleLogout();
         }
       }
     };
 
-    document.addEventListener('keydown', handleKeyPress);
+    if (loggedIn) {
+      document.addEventListener('keydown', handleKeyPress);
+    }
 
     return () => {
-      document.removeEventListener('keydown', handleKeyPress);
+      if (loggedIn) {
+        document.removeEventListener('keydown', handleKeyPress);
+      }
     };
-  }, [loggedIn]);
-
+  }, [loggedIn, gameOver]);
   const createUser = () => {
     if (username.trim() === '' || password.trim() === '') {
       toast.info('Por favor, preencha usuário e senha.');
       return;
     }
-  
+
     // Verifica se o usuário já existe
     if (localStorage.getItem(username)) {
       toast.error('Este usuário já existe. Por favor, escolha outro nome.');
       return;
     }
-  
-    // Aqui você pode adicionar lógica para criptografar a senha antes de armazená-la
+
     localStorage.setItem(username, password);
     toast.success(`Usuário ${username} criado com sucesso!`);
+    setShowCreateModal(false);
   };
 
   const login = () => {
@@ -100,7 +99,7 @@ const Home: React.FC = () => {
     setSequence(newSequence);
     setCurrentIndex(0);
     setGameOver(false);
-    setTimeLeft(20); // Reinicia o contador de tempo
+    setTimeLeft(20);
   };
 
   const handleKeyPress = (event: React.KeyboardEvent) => {
@@ -122,14 +121,13 @@ const Home: React.FC = () => {
 
   const handleCorrectSequence = () => {
     const newScore = score + sequence.length;
-    setScore(newScore); // Incrementa a pontuação com base no comprimento da sequência
-    localStorage.setItem(`${username}-score`, String(newScore)); // Salva a pontuação no localStorage
+    setScore(newScore);
+    localStorage.setItem(`${username}-score`, String(newScore));
 
     if (sequence.length < 10) {
-      // Para este exemplo, após 10 sequências corretas, você poderia considerar o jogador como vencedor.
       startGame();
     } else {
-      toast.success('Você ganhou o jogo!');
+      toast.success('Muite bem, continue assim!');
       startGame();
     }
   };
@@ -137,11 +135,11 @@ const Home: React.FC = () => {
   const handleGameOver = () => {
     toast.error('Game Over! Sua pontuação final: ' + score);
     setGameOver(true);
-    setScore(0); // Zera a pontuação
+    setScore(0);
   };
 
   const handleCreateUser = () => {
-    createUser();
+    setShowCreateModal(true);
   };
 
   const handleLogin = () => {
@@ -157,15 +155,18 @@ const Home: React.FC = () => {
   };
 
   const handleShowRanking = () => {
-    // Ordena os usuários pelo score em ordem decrescente
     const sortedScores = [...usersScores].sort((a, b) => b.score - a.score);
     setUsersScores(sortedScores);
     setShowModal(true);
   };
-  
+
 
   const handleCloseModal = () => {
     setShowModal(false);
+  };
+
+  const handleCloseCreateModal = () => {
+    setShowCreateModal(false);
   };
 
   const getAllUsersScores = () => {
@@ -186,36 +187,37 @@ const Home: React.FC = () => {
       <ToastContainer />
       {!loggedIn && (
         <>
-        <div className="mb-4 w-dvw flex flex-col items-center justify-center gap-4">
-          <input
-            type="text"
-            placeholder="Nome de usuário"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="border border-gray-300 rounded mr-2 px-3 py-1"
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="border border-gray-300 rounded mr-2 px-3 py-1"
-          />
-          <div className="flex flex-col gap-4 items-center justify-center">
-          <button onClick={handleCreateUser} className="px-4 py-1 bg-blue-500 text-white rounded">
-            Criar Usuário
-          </button>
-          <button onClick={handleLogin} className="px-4 py-1 bg-green-500 text-white rounded">
-            Login
-          </button>
+          <div className="mb-4 w-full md:w-1/2 flex flex-col items-center justify-center space-y-4">
+            <input
+              type="text"
+              placeholder="Nome de usuário"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="border border-gray-300 rounded px-3 py-1 w-full md:w-auto"
+            />
+            <input
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-gray-300 rounded px-3 py-1 w-full md:w-auto"
+            />
+            <div className="flex flex-col space-y-4 items-center justify-center">
+              <button onClick={handleLogin} className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none focus:bg-green-600">
+                Login
+              </button>
+              <p>Não tem uma conta?</p>
+              <button onClick={handleCreateUser} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+                Criar Usuário
+              </button>
+            </div>
           </div>
-        </div>
-        <div className='flex flex-col justify-center items-center'>
-          <button onClick={handleShowRanking} className="mt-4 px-4 py-2 w-1/2 bg-gray-500 text-white rounded">
-            Ver Ranking Geral
-          </button>
-          <p className='font-semibold text-lg'>Caso queira desconectar da sua conta pressione ESC</p>
-        </div>
+          <div className="flex flex-col justify-center items-center mt-4">
+            <button onClick={handleShowRanking} className="px-4 py-2 w-full md:w-1/2 bg-gray-500 text-white rounded hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Ver Ranking Geral
+            </button>
+            <p className="font-semibold text-lg mt-4">Caso queira desconectar da sua conta pressione ESC</p>
+          </div>
         </>
       )}
       {loggedIn && (
@@ -233,9 +235,7 @@ const Home: React.FC = () => {
             {sequence.map((key, index) => (
               <div
                 key={index}
-                className={`py-2 px-4 border rounded-lg cursor-pointer ${
-                  index === currentIndex ? 'bg-blue-500 text-white' : 'bg-gray-200'
-                }`}
+                className={`py-2 px-4 border rounded-lg cursor-pointer ${index === currentIndex ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
               >
                 {key}
               </div>
@@ -244,27 +244,24 @@ const Home: React.FC = () => {
           <div className="mt-8 text-lg text-red-600">
             {gameOver && 'Game Over! Pressione qualquer tecla para iniciar um novo jogo.'}
           </div>
-          {/* Elemento visível para capturar eventos de teclado */}
           <input
             type="text"
             className="opacity-100 bg-transparent border-none outline-none text-transparent w-0 h-0"
-            autoFocus // Foca automaticamente o input quando o componente é montado
+            autoFocus
             onKeyDown={handleKeyPress}
           />
-
         </>
       )}
 
-      {/* Modal de Ranking */}
       {showModal && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
           <div className="absolute w-full h-full bg-gray-900 opacity-50" onClick={handleCloseModal}></div>
           <div className="bg-white p-8 rounded-lg z-10">
-            <div className="w-full flex justify-between mb-4  gap-x-12">
-            <h2 className="text-2xl font-bold">Ranking Geral</h2>
-            <button onClick={handleCloseModal} className="font-bold border-2 rounded-full px-2 border-black">
-              X
-            </button>
+            <div className="w-full flex justify-between mb-4 gap-x-12">
+              <h2 className="text-2xl font-bold">Ranking Geral</h2>
+              <button onClick={handleCloseModal} className="font-bold border-2 rounded-full px-2 border-black">
+                X
+              </button>
             </div>
             <ul>
               {usersScores.map((user, index) => (
@@ -276,8 +273,40 @@ const Home: React.FC = () => {
           </div>
         </div>
       )}
+
+      {showCreateModal && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
+          <div className="absolute w-full h-full bg-gray-900 opacity-50" onClick={handleCloseCreateModal}></div>
+          <div className="bg-white p-8 rounded-lg z-10">
+            <h2 className="text-2xl font-bold mb-4">Criar Usuário</h2>
+            <input
+              type="text"
+              placeholder="Nome de usuário"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="border border-gray-300 rounded mr-2 px-3 py-1 mb-2"
+            />
+            <input
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border border-gray-300 rounded mr-2 px-3 py-1 mb-4"
+            />
+            <div className="flex justify-end">
+              <button onClick={createUser} className="px-4 py-1 bg-blue-500 text-white rounded">
+                Criar
+              </button>
+              <button onClick={handleCloseCreateModal} className="ml-2 px-4 py-1 bg-gray-300 text-black rounded">
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
+
 };
 
 export default Home;
